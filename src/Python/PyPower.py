2c9d594c4ccafd5589d3aada51675fad410f7ba7
jomariapc <jomariapc@gmail.com>
2023-12-05 19:42:29 +0100

    fix repo

--- src/Python/PyPower.py
+++ src/Python/PyPower.py
@@ -14,10 +14,12 @@ from itertools import combinations
 def check_nan(df: pd.DataFrame, show = True) -> None:
     
     """
-    Recibe un dataframe y enseña el % de nulos y lo grafica
+    Receives a dataframe and returns the % of nulls
+     
+    It plots it dependent on the show variable which has a default value of True.
     """
     
-    nan_cols = df.isna().mean() * 100  # % de valores nulos
+    nan_cols = df.isna().mean() * 100  # % of null values
     
     nan_cols = nan_cols[nan_cols>0]
     
@@ -29,14 +31,13 @@ def check_nan(df: pd.DataFrame, show = True) -> None:
     # Muestro el gráfico dependiendo de la variable show
     if(show):
 
-        #inicializa la figura
         plt.figure(figsize=(10, 6))  # 100X60  pixeles
 
 
-        sns.heatmap(df.isna(),       # datos
-                    yticklabels=False,  # quita las etiquetas del eje y
-                    cmap='viridis',      # mapa de color
-                    cbar=False           # sin barra lateral
+        sns.heatmap(df.isna(),       
+                    yticklabels=False,  
+                    cmap='viridis',      
+                    cbar=False           
                 )
 
         plt.show();
@@ -86,6 +87,19 @@ def replace_to(string, change, change_to):
 
 def column_counts(df):
 
+    """
+    Collects the number of unique values for each column in a DataFrame.
+
+    Iterates through each column, counts unique values, and groups columns by these counts.
+    Returns a sorted dictionary mapping the count of unique values to the corresponding column names.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame to evaluate.
+
+    Returns:
+    dict: Sorted dictionary with unique value counts as keys and lists of column names as values.
+    """
+
     dictio = {}
     columns = df.columns.tolist()
 
@@ -98,19 +112,33 @@ def column_counts(df):
 
 
 def drop_columns(df, columns):
+    """
+    Drops specified columns from a DataFrame.
 
+    Parameters:
+    df (pandas.DataFrame): The DataFrame from which to drop columns.
+    columns (list of str): List of column names to be dropped.
+
+    Returns:
+    pandas.DataFrame: DataFrame with specified columns removed.
+    """
     return df.drop(columns=columns)
+
 
 
 def change_type(df, column, type_from, type_to):
 
     """
-    Function to change a given column from a given DataFrame 
-    from 'type_from'
-    to 'type_to'
+    Converts the data type of a specified column in a DataFrame.
 
-    return the modified DataFrame
-    
+    Parameters:
+    df (pandas.DataFrame): The DataFrame containing the column to convert.
+    column (str): The name of the column to convert.
+    type_from (str): The original data type of the column.
+    type_to (str): The target data type to convert the column to.
+
+    Returns:
+    pandas.DataFrame: DataFrame with the column data type changed.
     """
 
     #If the type from is object
@@ -167,53 +195,68 @@ def change_type(df, column, type_from, type_to):
 # PROCESSING DATAFRAMES
 
 def group_and_drop_duplicates(df):
-    # Agrupa por las columnas relevantes y cuenta la cantidad de duplicados
+    """
+    Groups the DataFrame by relevant columns, counts duplicates, and consolidates them.
+
+    For each duplicate found based on 'artículos_ordenados', it combines the 'cantidad'
+    into the row with the maximum 'cantidad' and drops the remaining duplicates.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame to process.
+
+    Returns:
+    pandas.DataFrame: The DataFrame with duplicates consolidated and removed.
+    """
+    # Group by relevant columns and count the number of duplicates
     duplicates = df[df.duplicated(subset=['artículos_ordenados'], keep=False)]
 
-    # Itera sobre los duplicados
+    # Iterate over the duplicates
     for index, row in duplicates.iterrows():
-        # Encuentra las filas con la misma entrada en 'articulos_ordenados'
-        filas_similares = df[df['artículos_ordenados'] == row['artículos_ordenados']]
+        # Find rows with the same entry in 'artículos_ordenados'
+        similar_rows = df[df['artículos_ordenados'] == row['artículos_ordenados']]
 
-        # Encuentra la fila que tiene la cantidad máxima (la última en orden de aparición)
-        fila_max_cantidad = filas_similares.loc[filas_similares['cantidad'].idxmax()]
+        # Find the row with the maximum 'cantidad' (last in order of appearance)
+        max_qty_row = similar_rows.loc[similar_rows['cantidad'].idxmax()]
 
-        # Aumenta la cantidad en esa fila
-        df.at[fila_max_cantidad.name, 'cantidad'] += 1
+        # Increase the 'cantidad' in that row
+        df.at[max_qty_row.name, 'cantidad'] += 1
 
-        # Elimina la fila duplicada actual
+        # Drop the current duplicate row
         df.drop(index, inplace=True)
 
-    # Reinicia los índices después de las operaciones
-    return df.reset_index(drop=True, inplace=True)
+    # Reset the indices after the operations
+    df.reset_index(drop=True, inplace=True)
+    return df
 
 
-
-
-def new_table(df_from, column_to_check,  id_column_name, column_name,  id_list=[], create_id=True):
+def new_table(df_from, column_to_check, id_column_name, column_name, id_list=[], create_id=True):
 
     """
-    Function to create a new dataframe by creating a list of unique values 
-    from df_from[column_to_check] and in case create_id is True, creates a 
-    list of consecutive values for id, otherwise assigns the ids from id_list
-    
-    """
-    #Read the values of the column_to_check
-    data=list(df_from[column_to_check].unique())
-    
+    Creates a new DataFrame with unique values from a specified column of an existing DataFrame.
+    Optionally generates a list of consecutive IDs or uses a provided list of IDs.
 
-    #If create_id then create a list of consecutive values for the id
+    Parameters:
+    df_from (pandas.DataFrame): Source DataFrame.
+    column_to_check (str): Column name in df_from to extract unique values from.
+    id_column_name (str): Column name for the ID in the new DataFrame.
+    column_name (str): Column name for the unique values in the new DataFrame.
+    id_list (list, optional): List of IDs to use if create_id is False. Default is an empty list.
+    create_id (bool, optional): Flag to generate consecutive IDs. Default is True.
+
+    Returns:
+    pandas.DataFrame: A new DataFrame with two columns: IDs and unique values from the specified column.
+    """
+    # Read the unique values from the specified column
+    data = list(df_from[column_to_check].unique())
+
+    # Generate consecutive IDs if create_id is True
     if create_id:
-        ids=[]
+        ids = [str(e + 1) for e in range(len(data))]
+    else:
+        ids = id_list
 
-        for e in range(len(data)):
-            ids.append(str(e+1))
-
-    elif not create_id:
-        ids= id_list
-
-    #Create a dictionary with both lists
-    dict_temp= {id_column_name: ids, column_name: data}
+    # Create a dictionary with IDs and unique data
+    dict_temp = {id_column_name: ids, column_name: data}
 
     return pd.DataFrame(dict_temp)
 
@@ -221,16 +264,29 @@ def new_table(df_from, column_to_check,  id_column_name, column_name,  id_list=[
 
 #So proud of this function :D
 def fill_column(row, df_to_compare, column_to_compare, column_to_change):
+    
+    """
+    Updates a column value in a row based on matching values in another DataFrame.
 
+    This function checks if the value in the specified 'column_to_compare' of the row
+    exists in the same column of 'df_to_compare'. If a match is found, it updates the
+    value of 'column_to_change' in the row with the corresponding value from 'df_to_compare'.
+
+    Parameters:
+    row (pandas.Series): A row of a DataFrame where the update is to be performed.
+    df_to_compare (pandas.DataFrame): DataFrame to compare with for matching values.
+    column_to_compare (str): Column name to compare for matching values.
+    column_to_change (str): Column name in the row to be updated based on the match.
+
+    Returns:
+    pandas.Series: The updated row with the modified value in 'column_to_change'.
     """
-    
-    
-    """
-    
+    # Check if the value exists in the comparison DataFrame
     if row[column_to_compare] in df_to_compare[column_to_compare].values:
-        
+        # Find the matching row in the comparison DataFrame
         matching_row = df_to_compare[df_to_compare[column_to_compare] == row[column_to_compare]]
         
+        # Update the value of the specified column in the row
         row[column_to_change] = matching_row[column_to_change].values[0]
 
     return row
@@ -239,8 +295,19 @@ def fill_column(row, df_to_compare, column_to_compare, column_to_change):
 def fill_random(len, seed, choice, proportion):
 
     """
-    
-    
+    Generates a random array of specified length using given choices and proportions.
+
+    This function uses a seed for reproducibility and allows specifying the probability
+    distribution for the selection of elements.
+
+    Parameters:
+    len (int): Length of the array to be generated.
+    seed (int): Seed value for the random number generator for reproducibility.
+    choice (list): A list of elements from which the random selection will be made.
+    proportion (list): A list of probabilities associated with each element in 'choice'.
+
+    Returns:
+    numpy.ndarray: An array of randomly selected elements with the specified distribution.
     """
 
     np.random.seed(seed)
@@ -248,25 +315,61 @@ def fill_random(len, seed, choice, proportion):
     return np.random.choice(choice, size=len, p=proportion)
 
 
+
 def compare_fuzzywuzzy(lst, umbral=90):
+
+    """
+    Compares pairs of items in a list for similarity above a defined threshold.
+
+    This function utilizes the FuzzyWuzzy library to compute the similarity ratio
+    between each pair of items in the list. Pairs with a similarity ratio exceeding
+    the threshold are recorded.
+
+    Parameters:
+    lst (list): A list of items to be compared for similarity.
+    umbral (int, optional): The threshold for similarity ratio. Default is 90.
+
+    Returns:
+    list of tuples: A list containing tuples of the format (item1, item2, similarity_ratio)
+    for each pair where the similarity ratio exceeds the threshold.
+    """
 
     coincidencias = []
 
     for producto1, producto2 in combinations(lst, 2):
 
         similitud = fuzz.ratio(producto1, producto2)
-        
+
         if similitud > umbral:
+
             coincidencias.append((producto1, producto2, similitud))
 
     return coincidencias
 
+
 def update_item(cadena, lst, umbral=90):
+
+    """
+    Compares a given string against each item in a list using a similarity threshold. 
+
+    If the similarity ratio between the string and an item in the list exceeds the threshold,
+    the function returns the matching item from the list. If no matches are found above the
+    threshold, the original string is returned.
+
+    Parameters:
+    cadena (str): The string to be compared against the list.
+    lst (list): A list of strings to compare with.
+    umbral (int, optional): The threshold for the similarity ratio. Default is 90.
+
+    Returns:
+    str: The matching item from the list if similarity exceeds the threshold, otherwise the original string.
+    """
 
     for i in lst:
 
-        if fuzz.ratio(cadena, i)  > umbral:
+        if fuzz.ratio(cadena, i) > umbral:
 
             return i
-    
+        
     return cadena
+
--- src/Python/PySql.py
+++ src/Python/PySql.py
@@ -3,60 +3,96 @@ import pandas as pd
 #CREATE DICTIONARY WITH THE COLUMN NAMES AND THE COLUMN TYPES
 def create_dictio(df):
 
-    column_types= {}
+    """
+    Creates a dictionary mapping column names to their respective data types.
+
+    Iterates over each column in the provided DataFrame and categorizes its data type as 'FLOAT',
+    'INT', or 'VARCHAR(50)', based on the actual data type of the column.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame to analyze for column data types.
+
+    Returns:
+    dict: A dictionary with column names as keys and their categorized data types as values.
+    """
+
+    column_types = {}
 
     for e in df:
-
-        if ('float' in str(df[e].dtype)):
-            column_types[e]='FLOAT'
-
-        elif ('int' in str(df[e].dtype)):
-            column_types[e]='INT'
-
+        if 'float' in str(df[e].dtype):
+            column_types[e] = 'FLOAT'
+        elif 'int' in str(df[e].dtype):
+            column_types[e] = 'INT'
         else:
-            column_types[e]='VARCHAR(50)'
-        
+            column_types[e] = 'VARCHAR(50)'
 
     return column_types
+
         
 
 #CREATE TABLES
 def create_table(table_name, column_names_types, cursor):
+    
     """
-    Function that receives the name of the table "table_name", 
-    a dictionary "column_names_types" with the format 
-    key (column name): value(column type)  
+    Constructs and executes a SQL query to create a table with specified columns and types.
 
-    Returns the string with the query for create table
+    This function first drops the table if it already exists, then creates a new table with the
+    given name and columns as specified in the 'column_names_types' dictionary.
 
+    Parameters:
+    table_name (str): Name of the table to be created.
+    column_names_types (dict): Dictionary with column names as keys and their data types as values.
+    cursor (database cursor): Cursor object used to interact with the database.
+
+    Returns:
+    None: The function executes the SQL query but does not return any value.
     """
-
+    # Drop the table if it exists
     cursor.execute(f'DROP TABLE IF EXISTS `{table_name}`;\n')
     
-    # Add CREATE TABLE statement
+    # Start the CREATE TABLE statement
     query = f'CREATE TABLE `{table_name}`('
 
-    # Add all the columns to the string from the dictionary
+    # Add columns and their types to the query
     for key, value in column_names_types.items():
 
         query += f'{key} {value}, '
 
+    # Execute the final CREATE TABLE query
     cursor.execute(query[:-2] + ');')
 
+
 def create_tables(db_structure, cursor):
-        
+
+    """
+    Creates tables in a database based on a defined structure and populates them with data.
+
+    For each table in the 'db_structure' dictionary, the function reads data from a CSV file,
+    creates the table in the database, and sets up primary and foreign keys as specified.
+    It then populates the table with data from the CSV file.
+
+    Parameters:
+    db_structure (dict): A dictionary defining the structure of the database. Each key is a table name,
+                         and the value is a dictionary specifying primary and foreign keys.
+    cursor (database cursor): Cursor object used to interact with the database.
+
+    Returns:
+    None: The function executes SQL queries but does not return any value.
+    """
+
     for table, keys in db_structure.items():
-        #Read dataframe
+        # Read dataframe from CSV
         df = pd.read_csv(f'../../data/4-fill_db/{table}.csv')
-        #Create table with dataframe
+        # Create table in the database
         create_table(table, create_dictio(df), cursor)
         
-        # Check and add primary keys
+        # Add primary keys if defined
         if 'primary_keys' in keys:
             primary_keys_str = ', '.join([f'`{pk}`' for pk in keys['primary_keys']])
             primary_key_query = f'ALTER TABLE `{table}` ADD PRIMARY KEY ({primary_keys_str});'
             cursor.execute(primary_key_query)
 
+        # Add foreign keys if defined
         if 'foreign_keys' in keys:
             for fk_info in keys['foreign_keys']:
                 foreign_key_str = ', '.join([f'`{fk}`' for fk in fk_info['fk']])
@@ -64,21 +100,39 @@ def create_tables(db_structure, cursor):
                 foreign_key_query = f'ALTER TABLE `{table}` ADD FOREIGN KEY ({foreign_key_str}) REFERENCES {reference_str};'
                 cursor.execute(foreign_key_query)
 
-        #Insert values
+        # Insert values into the table
         insert_values(table, df, cursor)
+
 
 
 #INSERT VALUES
 def insert_values(table_name, df, cursor):
 
-    column_names = ','.join(df.columns)
+    """
+    Inserts data from a DataFrame into a specified table in the database.
 
-    # For each row
+    For each row in the DataFrame, the function constructs and executes an SQL insert query
+    to add the row to the specified table.
 
-    for i in range(df.shape[0]):   
+    Parameters:
+    table_name (str): Name of the table in the database where data will be inserted.
+    df (pandas.DataFrame): DataFrame containing data to be inserted into the table.
+    cursor (database cursor): Cursor object used to interact with the database.
+
+    Returns:
+    None: The function executes SQL insert queries but does not return any value.
+    """
+    
+    # Prepare column names for the SQL query
+    column_names = ','.join([f'`{col}`' for col in df.columns])
+
+    # Iterate over each row in the DataFrame
+    for i in range(df.shape[0]):
+        # Get values from the row as a tuple
+        values = tuple(df.iloc[i].values)
         
-        values = tuple(df.iloc[i].values)   
-        
-        cursor.execute(f'insert into `{table_name}` ({column_names}) values {values};')
+        # Construct and execute the insert query
+        cursor.execute(f'INSERT INTO `{table_name}` ({column_names}) VALUES {values};')
+
 
 
